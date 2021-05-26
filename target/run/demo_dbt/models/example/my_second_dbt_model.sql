bb9425c86f2dd85a4e932cae32695afef8bf87d7
Dhiraj Patra <dhiraj.patra@gmail.com>
2021-05-26 19:10:54 +0530

    first commit

---
+++ target/run/demo_dbt/models/example/my_second_dbt_model.sql
@@ -0,0 +1,8 @@
+
+  create or replace  view DEMO_DB.PUBLIC.my_second_dbt_model  as (
+    -- Use the `ref` function to select from other models
+
+select *
+from DEMO_DB.PUBLIC.my_first_dbt_model
+where id = 1
+  );
